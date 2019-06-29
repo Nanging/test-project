db74f533701e6187e33633d9db99d02832145592
Singke <q1006280595@163.com>
2019-06-29 18:27:03 +0800

    login

--- testdemo/src/main/java/com/sd/demo/config/SecurityConfig.java
+++ testdemo/src/main/java/com/sd/demo/config/SecurityConfig.java
@@ -1,17 +1,29 @@
 package com.sd.demo.config;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import com.sd.demo.support.AuthenticationAccessDeniedHandler;
 import com.sd.demo.support.LoginSuccessHandler;
@@ -48,23 +60,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 //		super.configure(http);
-		http.authorizeRequests()
-		.withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
-            @Override
-            public <O extends FilterSecurityInterceptor> O postProcess(O o) {
-                o.setSecurityMetadataSource(myFilterInvocationSecurityMetadataSource);
-                o.setAccessDecisionManager(myAccessDecisionManager);
-                return o;
-            }
-        })
+			System.out.println(" password " +new BCryptPasswordEncoder().encode("123"));
+			http.authorizeRequests()
+			.withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
+	            @Override
+	            public <O extends FilterSecurityInterceptor> O postProcess(O o) {
+	                o.setSecurityMetadataSource(myFilterInvocationSecurityMetadataSource);
+	                o.setAccessDecisionManager(myAccessDecisionManager);
+	                return o;
+	            }
+	        })
 			.antMatchers("/","/index","/register","/product/list","/login").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
 			.loginPage("/login")
-			.defaultSuccessUrl("/")
+			.loginProcessingUrl("/login").permitAll()
+			.failureHandler(new AuthenticationFailureHandler() {
+				@Override
+				public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+						AuthenticationException exception) throws IOException, ServletException {
+					// TODO Auto-generated method stub
+					response.setContentType("application/json;charset=utf-8");
+	                PrintWriter out = response.getWriter();
+	                StringBuffer sb = new StringBuffer();
+	                sb.append("{\"status\":\"error\",\"msg\":\"");
+	                if (exception instanceof UsernameNotFoundException || exception instanceof BadCredentialsException) {
+	                    sb.append("用户名或密码输入错误，登录失败!");
+	                } else if (exception instanceof DisabledException) {
+	                    sb.append("账户被禁用，登录失败，请联系管理员!");
+	                } else {
+	                    sb.append("登录失败!");
+	                }
+	                sb.append("\"}");
+	                out.write(sb.toString());
+	                out.flush();
+	                out.close();
+					
+				}
+	        })
 			.successHandler(new LoginSuccessHandler())
-			.failureUrl("/fail").permitAll()
 			.and()
 			.logout()
 			.logoutSuccessUrl("/login")
--- testdemo/src/main/java/com/sd/demo/support/LoginSuccessHandler.java
+++ testdemo/src/main/java/com/sd/demo/support/LoginSuccessHandler.java
@@ -1,6 +1,7 @@
 package com.sd.demo.support;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Set;
 
 import javax.servlet.ServletException;
@@ -11,24 +12,36 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class LoginSuccessHandler implements AuthenticationSuccessHandler{
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		// TODO Auto-generated method stub
-		Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        String path = request.getContextPath() ;
-        String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
-        for (String string : roles) {
-        	System.out.println("Permission : "+string);
-		}
-        if (roles.contains("AdminIndex")){
-            response.sendRedirect(basePath+"admin/index");
-            return;
-        }
-//        response.sendRedirect(basePath+"user/index");
-        response.sendRedirect(basePath+"product/list");
+		
+		response.setContentType("application/json;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String s = "{\"status\":\"success\",\"msg\":" + objectMapper.writeValueAsString("登录成功") + "}";
+        out.write(s);
+        out.flush();
+        out.close();
+		
+		
+//		Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+//        String path = request.getContextPath() ;
+//        String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+//        for (String string : roles) {
+//        	System.out.println("Permission : "+string);
+//		}
+//        if (roles.contains("AdminIndex")){
+//            response.sendRedirect(basePath+"admin/index");
+//            return;
+//        }
+////        response.sendRedirect(basePath+"user/index");
+//        response.sendRedirect(basePath+"product/list");
 	}
 
 }
